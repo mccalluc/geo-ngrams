cc163c704a64c3b5b608808266a71e1201c486ae
mccalluc <mccalluc@users.noreply.github.com>
2023-06-29 09:04:02 -0400

    change to bar chart

--- docs/js/visualization.js
+++ docs/js/visualization.js
@@ -25,24 +25,37 @@ async function main() {
       }
     ]
   }
-  const trellisSpec = {
+  // const scatterTrellisSpec = {
+  //   width: 60,
+  //   height: 60,
+  //   spacing: 0,
+  //   data: {values: ngrams},
+  //   mark: {type: 'text', fontSize: 8},
+  //   encoding: {
+  //     x: {field: 'norm', type: 'quantitative', title: null},
+  //     y: {field: 'count', type: 'quantitative', title: null},
+  //     text: {field: 'ngram'},
+  //     row: {field: 'lat', title: "latitude; count", sort: "descending"},
+  //     column: {field: "long", title: "longitude; log2(ratio)"}
+  //   }
+  // }
+  const barTrellisSpec = {
     width: 60,
     height: 60,
     spacing: 0,
     data: {values: ngrams},
-    mark: {type: 'text', fontSize: 8},
+    mark: {type: 'bar', cursor: 'pointer'},
     encoding: {
-      x: {field: 'norm', type: 'quantitative', title: null},
-      y: {field: 'count', type: 'quantitative', title: null},
-      text: {field: 'ngram'},
-      row: {field: 'lat', title: "latitude; count", sort: "descending"},
-      column: {field: "long", title: "longitude; log2(ratio)"}
+      x: {field: 'ngram', type: 'nominal', title: null},
+      y: {field: 'ratio', type: 'quantitative', title: null},
+      row: {field: 'lat', title: 'latitude', sort: "descending"},
+      column: {field: "long", title: 'longitude'}
     }
   }
 
   const spec = {
     $schema: "https://vega.github.io/schema/vega-lite/v5.json",
-    vconcat: [mapSpec, trellisSpec]
+    vconcat: [mapSpec, barTrellisSpec]
   };
   vegaEmbed('#vis', spec);
 }
